8d7096f748b3b9697d32eed1456db2f000296a65
Mirelly <mirelly.est.adv@gmail.com>
2026-03-31 22:37:51 -0300

    feat: projeto blog pessoal inicial

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { PostagemModule } from './postagem/postagem.modules';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+    type: "mysql",
+      host: "localhost",
+      port:  3306,
+      username: "root",
+      password: "root",
+      database: "db_blogpessoal",
+      entities: [Postagem],
+      synchronize: true,
+      logging: true,
+    }),
+    PostagemModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {}
